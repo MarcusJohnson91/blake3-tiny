18ebc2446aff9b74f3f61fdaeb4ceaeef44e67d5
Marcus Johnson <MarcusLJohnson1991@gmail.com>
2021-12-13 16:39:57 -0500

    Reformatted with Clang-Format

--- blake3.h
+++ blake3.h
@@ -4,11 +4,11 @@
 #include <stdint.h>
 
 struct blake3 {
-	unsigned char input[64];      /* current input bytes */
-	unsigned bytes;               /* bytes in current input block */
-	unsigned block;               /* block index in chunk */
-	uint64_t chunk;               /* chunk index */
-	uint32_t *cv, cv_buf[54 * 8]; /* chain value stack */
+    unsigned char input[64];          /* current input bytes */
+    unsigned      bytes;              /* bytes in current input block */
+    unsigned      block;              /* block index in chunk */
+    uint64_t      chunk;              /* chunk index */
+    uint32_t     *cv, cv_buf[54 * 8]; /* chain value stack */
 };
 
 void blake3_init(struct blake3 *);
